e19d46d565bc8a5cc0a7acd10c852813f9b985bb
khalid fifel <fifle.khalid99@gmail.com>
2023-05-04 18:53:46 +0100

    add miss assigne files

--- src/test/java/com/alibou/security/SecurityApplicationTests.java
+++ src/test/java/com/alibou/security/SecurityApplicationTests.java
@@ -1,13 +1,13 @@
-package com.spring.spring;
+package com.alibou.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SecurityApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
